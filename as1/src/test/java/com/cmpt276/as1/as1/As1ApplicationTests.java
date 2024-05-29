609370f0de7c4e71a099c9c46517337c9da9b4b4
Nolan Nadasdy <nolannadasdy@Nolans-MacBook-Air.local>
2024-05-29 12:22:18 -0700

    first commit

---
+++ as1/src/test/java/com/cmpt276/as1/as1/As1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmpt276.as1.as1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class As1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
